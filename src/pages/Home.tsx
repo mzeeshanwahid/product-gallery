573d3e2e82731d197d1d3760f343d55130034fd7
Muhammad Zeeshan <mzeeshan@emperiavr.com>
2025-08-07 23:49:08 +0500

    Homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,58 @@
+import { Box, Image, Text } from "@chakra-ui/react";
+import WelcomeImage from "../assets/images/welcome.png";
 import PrimaryLayout from "../layouts/PrimaryLayout";
+import { Link } from "react-router-dom";
+import LogoImg from "../assets/images/logo.png";
 
-function Homepage() {
+const HomePage = () => {
+  const visitEmperia = () => {
+    window.open("https://emperiavr.com/", "_blank", "noopener,noreferrer");
+  };
+
   return (
-    <>
-      <PrimaryLayout>
-        <p>Homepage</p>
-      </PrimaryLayout>
-    </>
+    <PrimaryLayout>
+      <Box
+        display="flex"
+        w="100%"
+        h="100%"
+        minH="80vh"
+        flexDirection="column"
+        gap="10"
+        alignItems="center"
+        justifyContent="center"
+        pb="50px"
+      >
+        <Image
+          src={WelcomeImage}
+          w="350px"
+          h="auto"
+          objectFit="contain"
+          mixBlendMode="darken"
+        />
+        <Box display="flex" alignItems="center" gap={3}>
+          <Text fontSize={"20px"} fontWeight="400" color="black">
+            Hi 👋,
+          </Text>
+          <Image
+            src={LogoImg}
+            width="150px"
+            height="auto"
+            objectFit="contain"
+            onClick={visitEmperia}
+            cursor="pointer"
+          />
+          <Text fontSize={"20px"} fontWeight="400" color="black">
+            Team
+          </Text>
+        </Box>
+        <Text fontSize={"20px"} fontWeight="400" color="gray">
+          Navigate to <Link to="/products">products page</Link> to explore
+          products
+        </Text>
+      </Box>
+    </PrimaryLayout>
   );
-}
+};
 
-export default Homepage;
+export default HomePage;
 
